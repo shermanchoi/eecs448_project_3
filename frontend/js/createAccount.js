7ba7f497b12b72e13796b5968b390bd865cc28a3
Doug Newman <douglasnewman987@gmail.com>
2018-04-02 23:30:03 -0500

    Added some validation functionality to createAccount; not yet finished

--- frontend/js/createAccount.js
+++ frontend/js/createAccount.js
@@ -1,8 +1,40 @@
 function generateDates() {
+    var today = new Date();
+    var year = today.getFullYear();
+
     var date_element = document.getElementById("Date");
-    var test = date_element.innerHTML;
-    for (int i = 1; i <= 31; i++) {
+    for (var i = 1; i <= 31; i++) {
 	date_element.innerHTML += "<option value=\"" + i + "\">" + i + "</option>\n";
     }
+
+    var year_element = document.getElementById("Year");
+    for (var i = (year - 100); i <= year; i++) {
+	year_element.innerHTML += '<option value=\"' + i + '\">' + i + '</option>\n';
+    }
 }
-    
+
+function formValidate() {
+    var errstatus = textBoxValidate() || dateValidate() || passwordValidate() || securityValidate();
+    if (errstatus) {
+	alert("Some parts of the form missing or invalid; please check form");
+	return (false);
+    }
+}
+
+function textBoxValidate() {
+    var textboxes = document.querySelectorAll('input[type="text"]');
+    var tbalert;
+    var errflag = 0;
+
+    for (var i = 0; i < textboxes.length; i++) {
+	if (textboxes[i].value == '') {
+	    document.getElementById('tb' + i + 'alert').style.display = "inline";
+	    errflag = 1;
+	}
+	else {
+	    document.getElementById('tb' + i + 'alert').style.display = "none";
+	}
+    }
+
+    return(errflag);
+}    
