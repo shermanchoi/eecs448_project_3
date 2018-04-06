6516374eec99395c7cc00ec1557f8def5e6cc056
William Graham <willgraham@live.com>
2018-04-05 21:56:57 -0500

    First push, figured stuff out

---
+++ src/main/java/team8/social/Main.java
@@ -0,0 +1,86 @@
+/*****
+ * Object: Main
+ * Description: 
+ * Author: William Graham
+ * Modified: April 04, 2018
+ *****/
+package team8.social;
+
+import java.util.Random;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Social Server Started\n");
+        configure();
+        
+        //define pages
+        pages();
+    }
+    
+    public static void configure(){
+        port(80);
+        staticFiles.location("/statics");
+        staticFiles.externalLocation("./resources");
+    }
+    
+    public static void pages(){
+        /*Handles the request to the root directory.*/
+        get("/", (req, res) -> {
+            if(req.session().attribute("SessionID") == null){
+                res.redirect("/login");
+            }else{
+                res.redirect("/home");
+            }
+            
+            return null;
+        });
+        
+        /*Handles the request to login*/
+        get("/login", (req, res) ->{
+            if(!validate(req.session().attribute("UserID"), req.session().attribute("SessionID"))) {
+                res.redirect("/login.html");
+            }else{
+                //send to home
+            }
+            return null;
+        });
+        post("/login", (req, res)->{
+            Integer userid = login(req.queryParams("username"), req.queryParams("password"));
+            Integer sessionid = (int) Math.random();
+            
+            if(userid != null){
+                req.session(true);
+                req.session().attribute("UserID", userid);
+                req.session().attribute("SessionID", sessionid);
+                return "Successfully Logged In";
+            }else{
+                res.redirect("login.html?error=invalid");
+            }
+            
+            return null;
+        });
+        
+        //Create Account
+        get("/createaccount", (req, res) -> {res.redirect("/createaccount.html"); return null;});
+    }
+    
+    
+    /**Logs the user in an creates a session.**/
+    public static Integer login(String username, String password){
+        try{
+            //Check database set id if success
+        }catch(Exception e) {
+    
+        }
+        
+        return null;
+    }
+    
+    /**Validates a session. (User is logged in)**/
+    public static boolean validate(String userid, String sessionid){
+        
+        return false;
+    }
+}
