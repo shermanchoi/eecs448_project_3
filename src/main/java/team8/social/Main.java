41fbc28909ceedcefba2277e9eeca3dc0a49c6eb
William Graham <willgraham@live.com>
2018-04-07 11:22:36 -0500

    Changed database

--- src/main/java/team8/social/Main.java
+++ src/main/java/team8/social/Main.java
@@ -21,7 +21,7 @@ public class Main {
     
     public static void configure(){
         port(80);
-        Database.initialize("jdbc:mysql://localhost:3306/sys", "root", "password");
+        Database.initialize("jdbc:mysql://mysql.eecs.ku.edu/w751g500", "w751g500", "eig4Jaix");
         staticFiles.location("/public");
         staticFiles.externalLocation("./resources");
     }
