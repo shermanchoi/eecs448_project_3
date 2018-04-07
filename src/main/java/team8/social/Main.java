3d4ded3318e928e5d400eeeebdac211744b5f54e
XZhou <x534z011@ku.edu>
2018-04-07 10:25:40 -0500

    Revert "Updated items to deal with data base."
    This reverts commit 37ebe4b1dc1b07c2bf9cf95958b38bebaa8de280.

--- src/main/java/team8/social/Main.java
+++ src/main/java/team8/social/Main.java
@@ -10,103 +10,92 @@ import java.util.Random;
 import static spark.Spark.*;
 
 public class Main {
-	public static void main(String[] args) {
-		try {
-			System.out.print("Social Server Started\n");
+    public static void main(String[] args){
+        System.out.print("Social Server Started\n");
+        
+        configure();
+        
+        //define pages
+        pages();
+    }
+    
+    public static void configure(){
+        port(80);
+        Database.initialize("jdbc:mysql://localhost:3306/sys", "root", "password");
+        staticFiles.location("/public");
+        staticFiles.externalLocation("./resources");
+    }
+    
+    /**
+     * Defines how URIs are handled.
+     * @post Server knows how to handle a number of URIs
+     */
+    public static void pages() {
+        /*Handles the request to the root directory.*/
+        get("/", (req, res) -> {
+            if (!Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))){
+                res.redirect("/login");
+            } else {
+                res.redirect("/home");
+            }
+    
+            return null;
+        });
+    
+        /*Handles the request to login*/
+        get("/login", (req, res) -> {
+            if (!Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
+                res.redirect("/html/login.html");
+                return null;
+            }
+            
+            res.redirect("/home");
+            return null;
+        });
+        post("/login", (req, res) -> {
+            if(Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))){
+               res.redirect("/home");
+               return null;
+            }
+            
+            String userid = Account.login((String) req.queryParams("username"), (String) req.queryParams("password")).getUsername();
+            Integer sessionid = (int) Math.random();
+    
+            if (userid != null) {
+                req.session(true);
+                req.session().attribute("UserID", userid);
+                req.session().attribute("SessionID", sessionid);
 
-			configure();
-
-			// define pages
-			pages();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public static void configure() {
-		port(80);
-		Database.initialize("jdbc:mysql://localhost:3306/sys", "root", "password");
-		staticFiles.location("/public");
-		staticFiles.externalLocation("./resources");
-	}
-
-	/**
-	 * Defines how URIs are handled.
-	 * 
-	 * @post Server knows how to handle a number of URIs
-	 */
-	public static void pages() {
-		/* Handles the request to the root directory. */
-		get("/", (req, res) -> {
-			if (!Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
-				res.redirect("/login");
-			} else {
-				res.redirect("/home");
-			}
-
-			return null;
-		});
-
-		/* Handles the request to login */
-		get("/login", (req, res) -> {
-			if (!Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
-				res.redirect("/html/login.html");
-				return null;
-			}
-
-			res.redirect("/home");
-			return null;
-		});
-		post("/login", (req, res) -> {
-			if (Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
-				res.redirect("/home");
-				return null;
-			}
-
-			String userid = Account.login((String) req.queryParams("username"), (String) req.queryParams("password"))
-					.getUsername();
-			Integer sessionid = (int) Math.random();
-
-			if (userid != null) {
-				req.session(true);
-				req.session().attribute("UserID", userid);
-				req.session().attribute("SessionID", sessionid);
-
-				res.redirect("/home");
-			} else {
-				res.redirect("html/login.html?error=invalid");
-			}
-
-			return null;
-		});
-
-		// Create Account
-		get("/createaccount", (req, res) -> {
-			if (!Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
-				res.redirect("/html/createaccount.html");
-				return null;
-			}
-
-			res.redirect("/home");
-			return null;
-		});
-		post("/createaccount", (req, res) -> {
-			if (Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
-				res.redirect("/home");
-				return null;
-			}
-
-			try {
-				String uname = req.queryParams("uname");
-				String pword = req.queryParams("pword");
-				String dateOfBirth = req.queryParams(queryParam);
-				Account a = Account.createAccount(uname, pword, dateOfBirth, fName, lName, secQ1, secQ2, secQ3, ansQ1, ansQ2, ansQ3);
-				
-				
-			} catch (Exception e) {
-
-			}
-			return null;
-		});
-	}
+                res.redirect("/home");
+            } else {
+                res.redirect("html/login.html?error=invalid");
+            }
+    
+            return null;
+        });
+    
+        //Create Account
+        get("/createaccount", (req, res) -> {
+            if(!Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))) {
+                res.redirect("/html/createaccount.html");
+                return null;
+            }
+            
+            res.redirect("/home");
+            return null;
+        });
+        post("/createaccount", (req, res) ->{
+            if(Session.validate(req.session().attribute("username"), req.session().attribute("sessionid"))){
+               res.redirect("/home");
+               return null;
+            }
+            
+            try{
+            
+            }catch(Exception e){
+            
+            }
+            return null;
+        });
+    }
 }
