a29b8b792d0a7bae6031a1348ef8fa629dd474b7
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2018-04-17 17:53:00 -0500

    Added more tests

--- src/test/java/TestAccountManagement.java
+++ src/test/java/TestAccountManagement.java
@@ -32,15 +32,47 @@ public class TestAccountManagement {
 	
 	@Test
 	public void testIncorrectPasswordLogin() {
-		//This should be a valid login due to password being incorrect
-		assertNotNull("Invalid Account Login Allowed (password is incorrect)",Account.login("schoi", "this is not the right password"));
+		//This should NOT be a valid login due to password being incorrect
+		assertNull("Invalid Account Login Allowed (password is incorrect)",Account.login("schoi", "this is not the right password"));
 	}
 	
 	@Test
 	public void testUsernameDoesNotExistLogin() {
-		//This should be a valid login due to username not existing
-		assertNotNull("Valid Account Login Failed (username does not exist)",Account.login("ishouldnotexist", "dictionaryAttacked"));
+		//This should NOT be allowed
+		assertNull("Invalid Account Login Allowed (username does not exist)",Account.login("ishouldnotexist", "dictionaryAttacked"));
 	}
 	
+	@Test
+	public void testChangePasswordThenLogin(){
+		//Successfully change password of schoi
+		Account.changePassword("schoi", "1234pass", "1234567890Password");
+		assertNotNull("Valid Account Login Failed (password change malfunctioned)",Account.login("schoi", "1234567890Password"));
+	}
 	
+	@Test
+	public void testChangePasswordFailure(){
+		//Attempt (but fail) to change the password of schoi
+		Account.changePassword("schoi", "1234pass", "newPassword");
+		assertNull("Invalid Account Login Allowed (password change malfunctioned)",Account.login("schoi", "newPassword"));
+	}
+	
+	@Test
+	public void testAnswerSecurityQuestionsCorrectly(){
+		//This should be true
+		assertTrue("Security question answers falsely denied",Account.securityQuestionCheck("schoi", "Pendulum", "SME", "Toy Story"));
+	}
+	
+	@Test
+	public void testAnswerSecurityQuestionsIncorrectly(){
+		//This should be false
+		assertFalse("Security question answers falsely accepted",Account.securityQuestionCheck("schoi", "SomethingBad", "OverTheRainbow", "Something"));
+	}
+	
+	@Test
+	public void testForceChangePassword(){
+		//Forcefully change the password of an account.
+		Account.changePassword("schoi", "newPassword");
+		//This should be a valid input now.
+		assertNotNull("Invalid Account Login Allowed (password change malfunctioned)",Account.login("schoi", "newPassword"));
+	}
 }
