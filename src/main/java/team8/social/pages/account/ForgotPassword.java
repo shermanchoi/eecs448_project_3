0e0e41e994a1b571e1ec548194f2b906922f3a73
William Graham <willgraham@live.com>
2018-04-23 06:54:55 -0500

    Added password reset functionality

--- src/main/java/team8/social/pages/account/ForgotPassword.java
+++ src/main/java/team8/social/pages/account/ForgotPassword.java
@@ -1,6 +1,5 @@
 package team8.social.pages.account;
 
-import sun.nio.cs.StandardCharsets;
 import team8.social.Account;
 import team8.social.PageHandler;
 import team8.social.Session;
@@ -14,23 +13,29 @@ import static spark.Spark.post;
 public class ForgotPassword implements PageHandler {
     private static String username = "";
     private static String security = "";
+    private static String password = "";
     
     public ForgotPassword(){
+        //Grab content of Username page
         try {
             username = new String(Files.readAllBytes(
                     Paths.get(getClass().getResource("/public/html/forgotPasswordUsernameInput.html").toURI())
             ));
-        }catch(Exception e){
+        }catch(Exception e){}
         
-        }
-        
+        //Grab content of Security Questions page
         try{
             security = new String(Files.readAllBytes(
                     Paths.get(getClass().getResource("/public/html/forgotPasswordSecQuestion.html").toURI())
             ));
-        }catch(Exception e){
+        }catch(Exception e){}
         
-        }
+        //Grab content of reset password page
+        try{
+            password = new String(Files.readAllBytes(
+                    Paths.get(getClass().getResource("/public/html/forgotPasswordReset.html").toURI())
+            ));
+        }catch(Exception e){}
     }
     
     public void pages() {
@@ -41,15 +46,7 @@ public class ForgotPassword implements PageHandler {
                 return null;
             }
             
-            if(req.session().attribute("forgot-username") == null){
-                //res.redirect("/html/forgotPasswordUsernameInput.html");
-                return username;
-            }else{
-                //res.redirect("/html/forgotPasswordSecQuestion.html");
-                return security;
-            }
-            
-            //return null;
+            return username;
         });
         
         post("/forgotpassword", (req, res)->{
@@ -58,17 +55,30 @@ public class ForgotPassword implements PageHandler {
                 return null;
             }
             
-            if(req.session().attribute("forgot-username") == null){
+            String state = req.session().attribute("forgot-state");
+            Boolean validated = req.session().attribute("forgot-validated") == "true";
+            String userid = req.session().attribute("forgot-username");
+            
+            if(state == "identify" || state == null){
                 String user = req.queryParams("uname");
                 if(Account.getSecurityQuestions(user) == null){
+                    req.session().attribute("forgot-state", "identify");
                     res.redirect("/forgotpassword");
                 }else{
+                    req.session().attribute("forgot-state", "security");
                     req.session().attribute("forgot-username", user);
-                    req.session().attribute("forgot-validated", "false");
-                    
-                    //res.redirect("/html/forgotPasswordSecQuestion.html");
                     return security;
                 }
+            }else if(state == "security" && userid != null){
+                
+                return password;
+            }else if(state == "reset" && validated){
+                if(req.queryParams("pword") != req.queryParams("cpword")){
+                    return password;
+                }
+                
+                Account.changePassword(userid, req.queryParams("pword"));
+                res.redirect("/home");
             }
             
             return null;
