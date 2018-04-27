77c63d3b5bd780783d3a9a519a076058ffa882b5
Doug Newman <douglasnewman987@gmail.com>
2018-04-27 09:45:29 -0500

    More changes to the myProfile

--- src/main/resources/public/js/myProfile.js
+++ src/main/resources/public/js/myProfile.js
@@ -1,14 +1,27 @@
 var editButton = document.getElementById("editbutton");
+var saveButton = document.getElementById("savebutton");
+var cancelButton = document.getElementById("cancelbutton");
+var bioText = document.getElementById("biotext");
+var bioTextContent = bioText.textContent;
+var bioEdit = document.getElementById("bioedit");
+bioEdit.value = bioTextContent.trim();
 editButton.addEventListener('click', editClick, false);
+cancelButton.addEventListener('click', cancelClick, false);
 
 function editClick() {
-    var bioText = document.getElementById("biotext");
-    var bioTextContent = bioText.textContent;
-    var bioEdit = document.getElementById("bioedit");
-    bioEdit.value = bioTextContent.trim();
-
     bioText.style.display = "none";
+    editButton.style.display = "none";
     bioEdit.style.display = "block";
+    cancelButton.style.display = "inline";
+    saveButton.style.display = "inline";
+}
+
+function cancelClick() {
+    bioEdit.style.display = "none";
+    cancelButton.style.display = "none";
+    saveButton.style.display = "none";
+    bioText.style.display = "block";
+    editButton.style.display = "inline";
 }
 
 
