37aa077f2179fdb270aa8f4eb32fd993a50db494
William Graham <willgraham@live.com>
2018-05-01 12:06:07 -0500

    Refactoring of how pages work

--- src/main/java/team8/social/pages/post/ViewPost.java
+++ src/main/java/team8/social/pages/post/ViewPost.java
@@ -1,9 +1,39 @@
 package team8.social.pages.post;
 
 import team8.social.PageHandler;
+import team8.social.Session;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static spark.Spark.get;
 
 public class ViewPost implements PageHandler {
-    public void pages() {
+    private static String view = "";
+    private static String reply = "";
     
+    public ViewPost(){
+        try{
+            view = new String(Files.readAllBytes(
+                    Paths.get(getClass().getResource("/public/html/postView.html").toURI())
+            ));
+        }catch(Exception e){}
+    
+        try{
+            reply = new String(Files.readAllBytes(
+                    Paths.get(getClass().getResource("/public/html/postViewReply.html").toURI())
+            ));
+        }catch(Exception e){}
+    }
+    
+    public void pages() {
+        get("/post", (req, res)->{
+            if (!Session.validate(req.session().id(), req.session().attribute("UserID"))) {
+                res.redirect("/login");
+                return null;
+            }
+            
+            return view;
+        });
     }
 }
