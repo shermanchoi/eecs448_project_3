0a0515db389d5d5f888fdc8e140ab8c017b9e65a
Sherman Choi <think88act@gmail.com>
2018-05-01 12:30:34 -0500

    Fixed readallbyte problem

--- src/main/java/team8/social/pages/post/ViewPost.java
+++ src/main/java/team8/social/pages/post/ViewPost.java
@@ -3,8 +3,11 @@ package team8.social.pages.post;
 import team8.social.PageHandler;
 import team8.social.Session;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import spark.utils.IOUtils;
 
 import static spark.Spark.get;
 
@@ -17,13 +20,27 @@ public class ViewPost implements PageHandler {
             view = new String(Files.readAllBytes(
                     Paths.get(getClass().getResource("/public/html/postView.html").toURI())
             ));
-        }catch(Exception e){}
+        }catch(Exception e){
+        	try {
+        		InputStream i = getClass().getResourceAsStream("/public/html/postView.html");
+        		view = new String(IOUtils.toByteArray(i));
+			} catch (Exception e2) {
+				
+			}
+        }
     
         try{
             reply = new String(Files.readAllBytes(
                     Paths.get(getClass().getResource("/public/html/postViewReply.html").toURI())
             ));
-        }catch(Exception e){}
+        }catch(Exception e){
+        	try {
+        		InputStream i = getClass().getResourceAsStream("/public/html/postViewReply.html");
+        		reply = new String(IOUtils.toByteArray(i));
+			} catch (Exception e2) {
+				
+			}
+        }
     }
     
     public void pages() {
