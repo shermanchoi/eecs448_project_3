27485e20bcf757f91c4d3fc1aa6fd57638e55416
XZhou <x534z011@ku.edu>
2018-05-02 08:39:51 -0500

    removed every BBCode related file

--- src/main/resources/public/js/BBCode/Parser/build/build.sh
+++
@@ -1,2 +0,0 @@
-tsc ../src/tokenizer.ts ../src/bbCodeParseTree.ts ../src/bbTag.ts ../src/bbCodeParser.ts
-uglifyjs ../src/tokenizer.js ../src/bbCodeParseTree.js ../src/bbTag.js ../src/bbCodeParser.js --output bbCodeParser.min.js
